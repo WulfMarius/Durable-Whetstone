07d9084c56efa06ef23d775ee8774466fd1f1092
WulfMarius <WulfMarius@users.noreply.github.com>
2017-08-12 18:02:53 +0200

    initial version

---
+++ DurableWhetstone.cs
@@ -0,0 +1,19 @@
+﻿using Harmony;
+
+namespace DurableWhetstone
+{
+    [HarmonyPatch(typeof(GearItem))]
+    [HarmonyPatch("Degrade")]
+    public class DurableWhetstone
+    {
+        static bool Prefix(GearItem __instance, ref float hp)
+        {
+            if (__instance && __instance.name == "GEAR_SharpeningStone")
+            {
+                hp = 1;
+            }
+
+            return true;
+        }
+    }
+}
